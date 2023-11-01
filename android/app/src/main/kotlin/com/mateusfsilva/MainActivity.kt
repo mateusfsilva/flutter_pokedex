6fb89453d6b360010201bf6df4ead22fb0fcac3a
Mateus Gustavo de Freitas e Silva <mateusfsilva@gmail.com>
2023-11-01 11:31:02 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/mateusfsilva/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mateusfsilva.flutter_pokedex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_pokedex/app/app.dart';
+import 'package:flutter_pokedex/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
